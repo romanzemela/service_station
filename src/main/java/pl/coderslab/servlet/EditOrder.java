fa4e23e90c750bf99b3f7f276dd64fafcb4166bc
Artur Siembab <ajtuss@gmail.com>
2018-07-31 16:00:58 +0200

    add post

--- src/main/java/pl/coderslab/servlet/EditOrder.java
+++ src/main/java/pl/coderslab/servlet/EditOrder.java
@@ -2,8 +2,10 @@ package pl.coderslab.servlet;
 
 import pl.coderslab.dao.EmployeeDAO;
 import pl.coderslab.dao.OrderDAO;
+import pl.coderslab.dao.VehicleDAO;
 import pl.coderslab.model.Employee;
 import pl.coderslab.model.Order;
+import pl.coderslab.model.Vehicle;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,13 +14,46 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @WebServlet("/orders/edit")
 public class EditOrder extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Integer id = Integer.parseInt(request.getParameter("id"));
+        String arrivalDate = request.getParameter("arrivalDate");
+        String plannedRepairDate = request.getParameter("plannedRepairDate");
+        String realRepairDate = request.getParameter("realRepairDate");
+        Integer employeeId = Integer.parseInt(request.getParameter("employee"));
+        String problemDescription = request.getParameter("problemDescription");
+        String repairDescription = request.getParameter("repairDescription");
+        String status = request.getParameter("status");
+        Integer vehicleId = Integer.parseInt(request.getParameter("vehicle"));
+        Double totalCost = Double.parseDouble(request.getParameter("totalCost"));
+        Double partsCost = Double.parseDouble(request.getParameter("partsCost"));
+        Integer workingHours = Integer.parseInt(request.getParameter("workingHours"));
 
-        getServletContext().getRequestDispatcher("/WEB-INF/editOrder.jsp").forward(request,response);
+
+        Date arrivalDateDate = null;
+        Date plannedRepairDateDate = null;
+        Date realRepairDateDate = null;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyy-MM-dd");
+        try {
+            arrivalDateDate = sdf.parse(arrivalDate);
+            plannedRepairDateDate = sdf.parse(plannedRepairDate);
+            realRepairDateDate = sdf.parse(realRepairDate);
+        } catch (ParseException ignore) {
+        }
+        try {
+            Order order = new Order(id, arrivalDateDate, plannedRepairDateDate, realRepairDateDate, EmployeeDAO.loadById(employeeId),
+                    problemDescription, repairDescription, status, VehicleDAO.loadById(vehicleId), totalCost, partsCost, workingHours);
+            OrderDAO.save(order);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        response.sendRedirect("/");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -28,9 +63,12 @@ public class EditOrder extends HttpServlet {
             id = Integer.parseInt(request.getParameter("id"));
             order = OrderDAO.loadById(id);
             List<Employee> employees = EmployeeDAO.loadAll();
+            List<Vehicle> vehicles = VehicleDAO.loadAll();
+
             request.setAttribute("order", order);
             request.setAttribute("employees", employees);
-            getServletContext().getRequestDispatcher("/WEB-INF/editOrder.jsp").forward(request,response);
+            request.setAttribute("vehicles", vehicles);
+            getServletContext().getRequestDispatcher("/WEB-INF/editOrder.jsp").forward(request, response);
         } catch (NumberFormatException ignore) {
         } catch (SQLException e) {
             e.printStackTrace();
