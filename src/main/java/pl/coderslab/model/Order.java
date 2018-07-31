78bb783e039e184c46e9ccd0f6d7869080189edb
Artur Siembab <ajtuss@gmail.com>
2018-07-31 10:15:59 +0200

    refactor variables and fix sqls exception

--- src/main/java/pl/coderslab/model/Order.java
+++ src/main/java/pl/coderslab/model/Order.java
@@ -4,45 +4,45 @@ import java.sql.Date;
 
 public class Order {
     private int id;
-    private Date arrival_date;
-    private Date planned_repair_date;
-    private Date real_repair_date;
-    private int employees_id;
-    private String problem_description;
-    private String repair_description;
+    private Date arrivalDate;
+    private Date plannedRepairDate;
+    private Date realRepairDate;
+    private Employee employee;
+    private String problemDescription;
+    private String repairDescription;
     private String status;
-    private int vehicles_id;
-    private float total_cost;
-    private float parts_cost;
-    private int working_hours;
+    private Vehicle vehicle;
+    private float totalCost;
+    private float partsCost;
+    private int workingHours;
 
-    public Order(Date arrival_date, Date planned_repair_date, Date real_repair_date, int employees_id, String problem_description, String repair_description, String status, int vehicles_id, float total_cost, float parts_cost, int working_hours) {
-        this.arrival_date = arrival_date;
-        this.planned_repair_date = planned_repair_date;
-        this.real_repair_date = real_repair_date;
-        this.employees_id = employees_id;
-        this.problem_description = problem_description;
-        this.repair_description = repair_description;
+    public Order(int id, Date arrivalDate, Date plannedRepairDate, Date realRepairDate, Employee employee, String problemDescription, String repairDescription, String status, Vehicle vehicle, float totalCost, float partsCost, int workingHours) {
+        this.id = id;
+        this.arrivalDate = arrivalDate;
+        this.plannedRepairDate = plannedRepairDate;
+        this.realRepairDate = realRepairDate;
+        this.employee = employee;
+        this.problemDescription = problemDescription;
+        this.repairDescription = repairDescription;
         this.status = status;
-        this.vehicles_id = vehicles_id;
-        this.total_cost = total_cost;
-        this.parts_cost = parts_cost;
-        this.working_hours = working_hours;
+        this.vehicle = vehicle;
+        this.totalCost = totalCost;
+        this.partsCost = partsCost;
+        this.workingHours = workingHours;
     }
 
-    public Order(int id, Date arrival_date, Date planned_repair_date, Date real_repair_date, int employees_id, String problem_description, String repair_description, String status, int vehicles_id, float total_cost, float parts_cost, int working_hours) {
-        this.id = id;
-        this.arrival_date = arrival_date;
-        this.planned_repair_date = planned_repair_date;
-        this.real_repair_date = real_repair_date;
-        this.employees_id = employees_id;
-        this.problem_description = problem_description;
-        this.repair_description = repair_description;
+    public Order(Date arrivalDate, Date plannedRepairDate, Date realRepairDate, Employee employee, String problemDescription, String repairDescription, String status, Vehicle vehicle, float totalCost, float partsCost, int workingHours) {
+        this.arrivalDate = arrivalDate;
+        this.plannedRepairDate = plannedRepairDate;
+        this.realRepairDate = realRepairDate;
+        this.employee = employee;
+        this.problemDescription = problemDescription;
+        this.repairDescription = repairDescription;
         this.status = status;
-        this.vehicles_id = vehicles_id;
-        this.total_cost = total_cost;
-        this.parts_cost = parts_cost;
-        this.working_hours = working_hours;
+        this.vehicle = vehicle;
+        this.totalCost = totalCost;
+        this.partsCost = partsCost;
+        this.workingHours = workingHours;
     }
 
     public int getId() {
@@ -53,52 +53,52 @@ public class Order {
         this.id = id;
     }
 
-    public java.sql.Date getArrival_date() {
-        return arrival_date;
+    public Date getArrivalDate() {
+        return arrivalDate;
     }
 
-    public void setArrival_date(Date arrival_date) {
-        this.arrival_date = arrival_date;
+    public void setArrivalDate(Date arrivalDate) {
+        this.arrivalDate = arrivalDate;
     }
 
-    public java.sql.Date getPlanned_repair_date() {
-        return planned_repair_date;
+    public Date getPlannedRepairDate() {
+        return plannedRepairDate;
     }
 
-    public void setPlanned_repair_date(Date planned_repair_date) {
-        this.planned_repair_date = planned_repair_date;
+    public void setPlannedRepairDate(Date plannedRepairDate) {
+        this.plannedRepairDate = plannedRepairDate;
     }
 
-    public java.sql.Date getReal_repair_date() {
-        return real_repair_date;
+    public Date getRealRepairDate() {
+        return realRepairDate;
     }
 
-    public void setReal_repair_date(Date real_repair_date) {
-        this.real_repair_date = real_repair_date;
+    public void setRealRepairDate(Date realRepairDate) {
+        this.realRepairDate = realRepairDate;
     }
 
-    public int getEmployees_id() {
-        return employees_id;
+    public Employee getEmployee() {
+        return employee;
     }
 
-    public void setEmployees_id(int employees_id) {
-        this.employees_id = employees_id;
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
     }
 
-    public String getProblem_description() {
-        return problem_description;
+    public String getProblemDescription() {
+        return problemDescription;
     }
 
-    public void setProblem_description(String problem_description) {
-        this.problem_description = problem_description;
+    public void setProblemDescription(String problemDescription) {
+        this.problemDescription = problemDescription;
     }
 
-    public String getRepair_description() {
-        return repair_description;
+    public String getRepairDescription() {
+        return repairDescription;
     }
 
-    public void setRepair_description(String repair_description) {
-        this.repair_description = repair_description;
+    public void setRepairDescription(String repairDescription) {
+        this.repairDescription = repairDescription;
     }
 
     public String getStatus() {
@@ -109,51 +109,35 @@ public class Order {
         this.status = status;
     }
 
-    public int getVehicles_id() {
-        return vehicles_id;
+    public Vehicle getVehicle() {
+        return vehicle;
     }
 
-    public void setVehicles_id(int vehicles_id) {
-        this.vehicles_id = vehicles_id;
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
     }
 
-    public float getTotal_cost() {
-        return total_cost;
+    public float getTotalCost() {
+        return totalCost;
     }
 
-    public void setTotal_cost(float total_cost) {
-        this.total_cost = total_cost;
+    public void setTotalCost(float totalCost) {
+        this.totalCost = totalCost;
     }
 
-    public float getParts_cost() {
-        return parts_cost;
+    public float getPartsCost() {
+        return partsCost;
     }
 
-    public void setParts_cost(float parts_cost) {
-        this.parts_cost = parts_cost;
+    public void setPartsCost(float partsCost) {
+        this.partsCost = partsCost;
     }
 
-    public int getWorking_hours() {
-        return working_hours;
+    public int getWorkingHours() {
+        return workingHours;
     }
 
-    public void setWorking_hours(int working_hours) {
-        this.working_hours = working_hours;
-    }
-
-    @Override
-    public String toString() {
-        return "Order{id=" + this.getId() +
-                ", arrival_date=" + this.getArrival_date() +
-                ", planned_repair_date=" + this.getPlanned_repair_date() +
-                ", real_repair_date=" + this.getReal_repair_date() +
-                ", emplyees_id=" + this.getEmployees_id() +
-                ", problem_description=" + this.getProblem_description() +
-                ", repair_description=" + this.getRepair_description() +
-                ", status=" + this.getStatus() +
-                ", vehicles_id=" + this.getVehicles_id() +
-                ", total_cost=" + this.getTotal_cost() +
-                ", parts_cost=" + this.getParts_cost() +
-                ", working_hours=" + this.getWorking_hours();
+    public void setWorkingHours(int workingHours) {
+        this.workingHours = workingHours;
     }
 }
