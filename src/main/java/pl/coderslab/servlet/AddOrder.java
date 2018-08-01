e35cac1a9573f6379feb6976c0723c566ce129fc
Artur Siembab <ajtuss@gmail.com>
2018-08-01 12:11:35 +0200

    add doPost metod

--- src/main/java/pl/coderslab/servlet/AddOrder.java
+++ src/main/java/pl/coderslab/servlet/AddOrder.java
@@ -1,8 +1,10 @@
 package pl.coderslab.servlet;
 
 import pl.coderslab.dao.EmployeeDAO;
+import pl.coderslab.dao.OrderDAO;
 import pl.coderslab.dao.VehicleDAO;
 import pl.coderslab.model.Employee;
+import pl.coderslab.model.Order;
 import pl.coderslab.model.Vehicle;
 
 import javax.servlet.ServletException;
@@ -12,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -21,8 +24,31 @@ public class AddOrder extends HttpServlet {
     SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String arrivalDate = request.getParameter("arrivalDate");
+        String plannedRepairDate = request.getParameter("plannedRepairDate");
+        Integer employeeId = Integer.parseInt(request.getParameter("employee"));
+        String problemDescription = request.getParameter("problemDescription");
+        String status = "Przyjęty";
+        Integer vehicleId = Integer.parseInt(request.getParameter("vehicle"));
 
+        Date arrivalDateDate = null;
+        Date plannedRepairDateDate = null;
+        try {
+            arrivalDateDate = sdf.parse(arrivalDate);
+            plannedRepairDateDate = sdf.parse(plannedRepairDate);
+        } catch (ParseException ignore) {
+        }
+        try {
+            Order order = new Order(arrivalDateDate, plannedRepairDateDate, EmployeeDAO.loadById(employeeId),
+                    problemDescription, status, VehicleDAO.loadById(vehicleId));
+            OrderDAO.save(order);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        response.sendRedirect("/");
     }
+
+
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
