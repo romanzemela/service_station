970bb81454bde70b778ead9bfe8360e523143810
Artur Siembab <ajtuss@gmail.com>
2018-08-02 14:14:29 +0200

    fix servlet path

--- src/main/java/pl/coderslab/servlet/Raport1.java
+++ src/main/java/pl/coderslab/servlet/Raport1.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("raport1")
+@WebServlet("/raport1")
 public class Raport1 extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
