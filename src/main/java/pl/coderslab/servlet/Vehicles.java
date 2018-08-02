6360772b0e71df06c1848d8c2d1f3c753c37ac7f
Artur Siembab <ajtuss@gmail.com>
2018-08-02 09:56:28 +0200

    add attribute customer list

--- src/main/java/pl/coderslab/servlet/Vehicles.java
+++ src/main/java/pl/coderslab/servlet/Vehicles.java
@@ -1,6 +1,8 @@
 package pl.coderslab.servlet;
 
+import pl.coderslab.dao.CustomerDAO;
 import pl.coderslab.dao.VehicleDAO;
+import pl.coderslab.model.Customer;
 import pl.coderslab.model.Vehicle;
 
 import javax.servlet.ServletException;
@@ -10,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Collections;
 import java.util.List;
 
 @WebServlet("/vehicles")
@@ -17,15 +20,32 @@ public class Vehicles extends HttpServlet {
 
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-
+        Integer customerId = null;
+        List<Vehicle> vehicles = Collections.emptyList();
         try {
-            List<Vehicle> vehicles = VehicleDAO.loadAllByCustomerId(Integer.parseInt(request.getParameter("customerId")));
-            request.setAttribute("vehicles", vehicles);
+            customerId = Integer.parseInt(request.getParameter("customerId"));
         } catch (NumberFormatException ignore) {
+        }
+        try {
+            if (customerId == null) {
+                vehicles = VehicleDAO.loadAll();
+
+            } else {
+                vehicles = VehicleDAO.loadAllByCustomerId(customerId);
+
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        request.setAttribute("vehicles", vehicles);
+
+        try {
+            List<Customer> customers = CustomerDAO.loadAll();
+            request.setAttribute("customers", customers);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         getServletContext().getRequestDispatcher("/WEB-INF/vehicles.jsp").forward(request, response);
 
     }
